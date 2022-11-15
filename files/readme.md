9659e5c0a4e6ac77cda1809b387ef727eb46c666
Kristen Wells <kristen.wells@cuanschutz.edu>
2022-11-15 13:17:33 -0700

    initial commit

---
+++ files/readme.md
@@ -0,0 +1,5 @@
+# File information
+
+`baby_names.xlsx` was manually downloaded from https://www.ssa.gov/cgi-bin/namesbystate.cgi
+
+I downloaded through Maryland for 2020 - decide if I want to do more...
---
+++ make_data_frames.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+library(openxlsx)
+library(here)
+
+data_path <- here("files/baby_names.xlsx")
+
+all_states <- openxlsx::getSheetNames(data_path)
+
+all_names <- lapply(all_states, function(x){
+  names_data <- openxlsx::readWorkbook(xlsxFile = data_path,
+                                       sheet = x)
+  colnames(names_data) <- c(paste0(x, "_rank"),
+                            "Male_name",
+                            paste0(x, "_number_of_males"),
+                            "Female_name",
+                            paste0(x, "_number_of_females"))
+
+  return(names_data)
+})
+
+names(all_names) <- all_states
+
+boy_names <- lapply(all_names, function(x){
+  return(x[ , c(2, 3)])
+})
+
+boy_names <- boy_names %>%
+  reduce(inner_join, by = "Male_name")
+
+write.csv(boy_names, here("files/boy_name_counts.csv"))
+
+girl_names <- lapply(all_names, function(x){
+  return(x[ , c(4, 5)])
+})
+
+girl_names <- girl_names %>%
+  reduce(inner_join, by = "Female_name")
+
+write.csv(girl_names, here("files/girl_name_counts.csv"))
+
+boy_ranks <- lapply(all_names, function(x){
+  return(x[ , c(1, 2)])
+})
+
+boy_ranks <- boy_ranks %>%
+  reduce(inner_join, by = "Male_name")
+
+write.csv(boy_ranks, here("files/boy_name_ranks.csv"))
+
+
+girl_ranks <- lapply(all_names, function(x){
+  return(x[ , c(1, 4)])
+})
+
+girl_ranks <- girl_ranks %>%
+  reduce(inner_join, by = "Female_name")
+
+write.csv(girl_ranks, here("files/girl_name_ranks.csv"))
+
+
